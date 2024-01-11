ab28ae1ebbbaeeb13ec5dda1a273e62bda2f1865
mahermorsi <mahermorsi@gmail.com>
2024-01-11 22:58:20 +0200

    implemented authentication setup for login, and login test

--- infra/api/apiRequest.ts
+++ infra/api/apiRequest.ts
@@ -1,4 +1,4 @@
-import { request } from "@playwright/test"
+import { APIRequestContext, request } from "@playwright/test"
 
 
 const putRequest = async (url:string,body:any) => {
@@ -8,13 +8,19 @@ const putRequest = async (url:string,body:any) => {
    })      
 }
 
-const postRequest = async (url:string,body:any) => {
-    const myRequest = await request.newContext()
-    return await myRequest.post(url,{
-    data:body,
-   })      
-}
-
+const postRequest = async (url: string, body: any, headers?: Record<string, string>,availableRequest?:APIRequestContext) => {
+    const requestOptions: Record<string, any> = {
+        data: body,
+    };
+    if (headers) {
+        requestOptions.headers = headers;
+    }
+    if (availableRequest){
+        return await availableRequest.post(url,requestOptions);
+    }
+    const newRequest = await request.newContext();
+    return await newRequest.post(url, requestOptions);
+};
 
 const patchRequest = async (url:string,) => {
     const myRequest = await request.newContext()
