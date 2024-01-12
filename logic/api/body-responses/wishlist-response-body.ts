06344b0401eba6deec2b1cdd4897496ded423c00
mahermorsi <mahermorsi@gmail.com>
2024-01-12 17:22:20 +0200

    added enum, wrapped http response, and cleaned up items after each test

---
+++ logic/api/body-responses/wishlist-response-body.ts
@@ -0,0 +1,20 @@
+export interface WishlistResponse {
+    data: {
+        addProductsToWishlist: {
+            changed: number;
+            anyWishlist: {
+                items_count: number;
+                items: WishlistItem[];
+            };
+        };
+    };
+}
+
+export interface WishlistItem {
+    id: number;
+}
+
+export async function wrapWishlistResponse(responseJson: any): Promise<WishlistResponse | null> {
+        return await responseJson.json()
+
+}
