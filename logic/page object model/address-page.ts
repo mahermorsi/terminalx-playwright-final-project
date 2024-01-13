3db9e55376f61e042d38de700a904ca7501faa6b
mahermorsi <mahermorsi@gmail.com>
2024-01-13 02:36:48 +0200

    implemented address test

---
+++ logic/page object model/address-page.ts
@@ -0,0 +1,21 @@
+import {type Locator, type Page } from '@playwright/test';
+import { BasePage } from '../../infra/ui/base-page';
+import { waitForElementToBeVisible } from '../../utils/wait-for-elements';
+
+
+
+export class AddressPage extends BasePage{
+    private readonly addressList: Locator
+
+    constructor(page: Page){
+        super(page)
+        this.initPage();
+        this.addressList=this.page.locator('//tbody/tr')
+    }
+    
+    async getTotalCountOfAddresses(){
+        const result = await waitForElementToBeVisible(this.addressList.first(),500,5)
+        if (!result) {throw new Error("Locator isn't visible")}
+        return await this.addressList.count()
+    }
+}
