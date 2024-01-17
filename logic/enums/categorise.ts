dd9e76970eb3862aaba8c04f63ebf83e73e90b61
saeed <107278609+SaeedYasen@users.noreply.github.com>
2024-01-17 09:43:11 +0200

    [Parameterized] test in main page ->
    [Parameterized] in main page -> click on [men, women, kids. for you] -> validate we are navigated to the corresponding page

---
+++ logic/enums/categorise.ts
@@ -0,0 +1,5 @@
+export enum Category {
+    WOMEN = "WOMEN",
+    MEN = "MEN",
+    KIDS = "KIDS"
+  }
--- logic/page object model/header-component.ts
+++ logic/page object model/header-component.ts
@@ -1,25 +1,34 @@
-import {type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test';
 import { BasePage } from '../../infra/ui/base-page';
+import { waitForElementToBeVisible } from '../../utils/utils'
+import { error } from 'console';
 
-export class HeaderComponent extends BasePage{
+export class HeaderComponent extends BasePage {
     private readonly headersList: Locator
+    private readonly categoriseList: Locator
 
-    constructor(page: Page){
+    constructor(page: Page) {
         super(page)
         this.initPage();
-        this.headersList=this.page.locator('//ul[@class="list_8stB"]//li/a')
+        this.headersList = this.page.locator('//ul[@class="list_8stB"]//li/a')
+        this.categoriseList = this.page.locator('//div[@class="landing-page-buttons-section"]')
     }
 
-    ClickHeaderItemByIndex = async (i:number) => {
+    ClickOnCategory = async (category: string) => {
+        const result = await waitForElementToBeVisible(this.categoriseList.first())
+        if (!result) throw new Error("the locator isn't visibile")
+        await this.categoriseList.locator(`//a[text()="${category}"]`).click()
+    }
+    ClickHeaderItemByIndex = async (i: number) => {
         const href = await this.getHrefByIndex(i)
         await this.headersList.nth(i).click()
         return href
     }
 
-    private getHrefByIndex = async (i:number) => {
+    private getHrefByIndex = async (i: number) => {
         return await this.headersList.nth(i).getAttribute('href')
     }
-    
+
     getButtonsNumber = async () => {
         return await this.headersList.count()
     }
