ebaed98087ba5fabb0fc5b84f99fb04418b299cd
mahermorsi <mahermorsi@gmail.com>
2024-01-18 07:18:28 +0200

    added tests to cover all sales filter

--- logic/enums/sale-percentages.ts
+++ logic/enums/sale-percentages.ts
@@ -1,6 +1,10 @@
 export enum SalePercentages {
+    FiftenPercentOff = "15% OFF",
+    TwentyFivePercentOff = "25% OFF",
     TwentyPercentOff = "20% OFF",
     ThirtyPercentOff = "30% OFF",
     FortyPercentOff = "40% OFF",
     FiftyPercentOff = "50% OFF",
+    SixtyPercentOff = "60% OFF",
+    SeventyPercentOff = "70% OFF"
 }
